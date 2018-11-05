c93a922f3b23812c1056c76ea43d080be459f504
Marissa Borrego <marbborrego@gmail.com>
2018-11-04 23:07:06 -0800

    Update forest/hforest_test.cc

---
+++ forest/hforest_test.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "hforest.cc"
+#include <vector>
+
+HTree::tree_ptr_t a(new HTree::HTree(6, 13));
+
+HTree::tree_ptr_t c(new HTree::HTree(12, 17));
+
+HTree::tree_ptr_t b(new HTree::HTree(-5, 14, c, a));
+
+HTree::tree_ptr_t d(new HTree::HTree(3, 76));
+
+HTree::tree_ptr_t e(new HTree::HTree(12, 3, d, nullptr));
+
+HTree::tree_ptr_t first(new HTree::HTree(126, 0, b, e));
+
+HTree::tree_ptr_t add_this(new HTree::HTree(13, 4));
+
+
+HForest::tree_vec trees = {a, b, c , d, e, first};
+HForest::tree_vec empty = {};
+
+
+HForest forest(trees);
+
+int main(){
+  auto s = forest.size();
+  std::cout << "Forest size is "<< s << " trees \n";
+  s = empty.size();
+  std::cout << "Empty forest is " << s << "\n";
+
+  forest.add_tree(add_this);
+  s = forest.size();
+  std::cout << "New size is " << s << "\n";
+
+  auto popped = forest.pop_tree();
+  std::cout << "Popped has value of " << popped->get_value() << "\n";
+
+  popped = forest.pop_tree();
+  std::cout << "Popped has value of " << popped->get_value() << "\n";
+
+  s = forest.size();
+  std::cout << "New size is " << s << "\n";
+}
