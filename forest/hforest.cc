84b6c93bd33c6449841f5c235740f1227b98568c
Marissa Borrego <marbborrego@gmail.com>
2018-11-04 23:29:53 -0800

    Update forest/hforest.cc

--- forest/hforest.cc
+++ forest/hforest.cc
@@ -8,25 +8,30 @@ HForest::HForest(HForest::tree_vec trees)
 {
   
 }
-double HForest::size() {
+
+//returns number of trees in the vector
+double HForest::size() {     // Relies on std::vector built_in size()
   return trees_.size();
 }
 
+// Adds a new tree to the end of the vector
 void HForest::add_tree (HTree::tree_ptr_t tree){
-  trees_.push_back(tree);
+  trees_.push_back(tree);         // Relies on std::vector built_in push_bak()
 }
 
+// Helper function for tree_ptr_to
 bool compare_trees(HTree::tree_ptr_t t1, HTree::tree_ptr_t t2) {
-  return t1->get_value() < t2->get_value();
+  return t1->get_value() < t2->get_value();          // Trees are ordered by the size of their value, with the hghest valued tree being the root
 }
 
+// Removes and returns the tree containing the highest value
 HTree::tree_ptr_t HForest::pop_tree(){
-  if (trees_.size() == 0){
+  if (trees_.size() == 0){               // Cant pop anything if the vector is empty
     return nullptr;
   }
-  std::make_heap(trees_.begin(), trees_.end(), compare_trees);
-  std::pop_heap(trees_.begin(), trees_.end());
-  auto largest = trees_.back();
-  trees_.pop_back();
+  std::make_heap(trees_.begin(), trees_.end(), compare_trees); // Lazy-order of vector makes the first tree the one with the highest value
+  std::pop_heap(trees_.begin(), trees_.end());                 // Places the largest valued tree last
+  auto largest = trees_.back();                                // saves the largest valued tree
+  trees_.pop_back();                                           // removes the largest valued tree
   return largest;
 }
