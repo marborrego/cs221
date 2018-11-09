e7ecbd4ca910b7ed0beefb4453e4ed6bb9f38f35
Marissa Borrego <marbborrego@gmail.com>
2018-11-09 13:43:21 -0800

    Update huffman/bitio.cc

---
+++ huffman/bitio.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "bitio.hh"
+
+  // Construct with one of either an input stream or output (the other null)
+  BitIO(std::ostream* os, std::istream* is);
+
+  // Flushes out any remaining output bits and trailing zeros, if any:
+  ~BitIO();
+
+  BitIO(const BitIO&) = default;
+  BitIO(BitIO&&) = default;
+  BitIO& operator=(const BitIO&) = default;
+  BitIO& operator=(BitIO&&) = default;
+
+  // Output a single bit (buffered)
+  void output_bit(bool bit);
+
+  // Read a single bit (or trailing zero)
+  bool input_bit();
