b68e5217fecc67476bd82670d3981d5eb30250ca
Marissa Borrego <marbborrego@gmail.com>
2018-11-10 18:16:49 -0800

    Update huffman/bitio.cc

--- huffman/bitio.cc
+++ huffman/bitio.cc
@@ -1,15 +1,16 @@
-#include <iostream>
+//#include <iostream>
 #include "bitio.hh"
 #include <cassert>
 #include <math.h>
 
 BitIO::BitIO(std::ostream* os, std::istream* is)
-  : os_(os), is_(is), input_index(0), output_index(7), output_char(0), current_char(get_next_char())
+  : os_(os), is_(is), input_index(0), output_index(0), output_char(0), current_char(0)
 {
   assert((os_ || is_)&& !(os_ && is_));
+  if (is_){
+    get_next_char();
+  }
 }
-
-  // Flushes out any remaining output bits and trailing zeros, if any:
 
   // Output a single bit (buffered)
 void BitIO::output_bit(bool bit){
@@ -17,9 +18,9 @@ void BitIO::output_bit(bool bit){
     output_char = output_char + pow(2, output_index);
   }
   -- output_index;
-  if (output_index == -1){
+  if (output_index == 8){
     *os_ << output_char;
-    output_index = 7;
+    output_index = 0;
   }
 }
 
@@ -29,9 +30,16 @@ bool BitIO::input_bit(){
     input_index = 0;
     get_next_char();
   }
-
+  if (current_char % 2 == 0){
+    current_char = current_char >> 1;
+    ++ input_index;
+    return false;
+  }
+  current_char = current_char >> 1;
+  ++ input_index;
+  return true;
 }
 
-int BitIO::get_next_char(){
+void BitIO::get_next_char(){
   *is_ >> current_char;
 }
