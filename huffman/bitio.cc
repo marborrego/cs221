a92c4b59d0029d16896bd2f73204362f1cd50929
Marissa Borrego <marbborrego@gmail.com>
2018-11-11 16:03:59 -0800

    Update huffman/bitio.cc

--- huffman/bitio.cc
+++ huffman/bitio.cc
@@ -6,31 +6,38 @@
 BitIO::BitIO(std::ostream* os, std::istream* is)
   : os_(os), is_(is), input_index(0), output_index(0), output_char(0), current_char(0)
 {
-  assert((os_ || is_)&& !(os_ && is_));
+  assert((os_ || is_)&& !(os_ && is_));    // Exactly one stream can be inputed
   if (is_){
-    get_next_char();
+    get_next_char();                       // Sets up the first character to read
+  }
+}
+
+BitIO::~BitIO(){
+  if (os_ && output_index != 0){
+    *os_ << output_char;                   // Pushes the char if there was a number of bits not mod 8
   }
 }
 
   // Output a single bit (buffered)
 void BitIO::output_bit(bool bit){
   if (bit == 1){
-    output_char = output_char + pow(2, output_index);
+    output_char = output_char + pow(2, output_index); // Only changes the output_char when bit is true
   }
-  -- output_index;
-  if (output_index == 8){
+  ++ output_index;
+  if (output_index == 8){         // Pushes and resets out_put char once each bit has been accounted for
     *os_ << output_char;
+    output_char = 0;
     output_index = 0;
   }
 }
 
   // Read a single bit (or trailing zero)
 bool BitIO::input_bit(){
-  if (input_index == 8){
+  if (input_index == 8){          // Gets a new char once all of the bits from the last one were read
     input_index = 0;
     get_next_char();
   }
-  if (current_char % 2 == 0){
+  if (current_char % 2 == 0){               // If the character is divisible by two, that means its least significant bit is a 0
     current_char = current_char >> 1;
     ++ input_index;
     return false;
@@ -40,6 +47,7 @@ bool BitIO::input_bit(){
   return true;
 }
 
+// Helper function for input_bit loads in a character from is_ to current_char
 void BitIO::get_next_char(){
-  *is_ >> current_char;
+  is_->get(current_char);
 }
