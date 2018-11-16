d9358814311144e10b299162a6825427323e8ee8
Marissa Borrego <marbborrego@gmail.com>
2018-11-15 23:01:24 -0800

    Update tsm/tsp.cc

---
+++ tsm/tsp.cc
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include "cities.hh"
+
+int main(int argc, char *argv[]){
+  std::cout << argc << "\n";
+  for (int i = 1; i < argc; i++){                                                    // Allows for multiple files to be read
+    std::ifstream infile {argv[i]};
+    std::cout << "made it here 1! \n";
+    assert(infile);
+    std::ofstream outfile {"shortest.tsv"};
+    assert(outfile);
+
+    Cities city;
+    infile >> city;
+    double best_dist = std::numeric_limits<double>::infinity();
+    for (int i = 0; i < 1000000; i++){
+      Cities::cities city_vec = city.get_cities();
+      unsigned len = city_vec.size();
+      Cities::permutation_t permutation = city.random_permutation(len);
+      double this_dist = city.total_path_distance(permutation);
+      if (this_dist < best_dist){
+        best_dist = this_dist;
+        city = city.reorder(permutation);
+        std::cout << double(i) << " " << int(best_dist) << "\n";
+      }
+    }
+    outfile << city;
+    infile.close();
+    outfile.close();
+  }
+  return 0;
+}
