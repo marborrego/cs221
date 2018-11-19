f3637ce3648823256b58f4ea0c24564d8e49629b
Marissa Borrego <marbborrego@gmail.com>
2018-11-18 16:21:17 -0800

    Update tsp.cc

--- tsm/tsp.cc
+++ tsm/tsp.cc
@@ -9,13 +9,13 @@ int main(int argc, char *argv[]){
   for (int i = 1; i < argc; i++){                                                    // Allows for multiple files to be read
     std::ifstream infile {argv[i]};
     assert(infile);
-    std::ofstream outfile {"shortest.tsv"};
+    std::ofstream outfile {"shortest.tsv"};                                          // The shortest path found will be outputted as ordered coordinates 
     assert(outfile);
 
     Cities city;
-    infile >> city;
-    double best_dist = std::numeric_limits<double>::infinity();
-    for (int i = 0; i < 1000000; i++){
+    infile >> city;                                                                  // Stream in cordinates of cities
+    double best_dist = std::numeric_limits<double>::infinity();                      // The best distance is initialized to infinity so any distance is better
+    for (int i = 0; i < 1000000; i++){                                               // This is the main part of the program, where the coordinates are randomly ordered and the distance is measured. The shortest distance is saved and outputted.
       Cities::cities city_vec = city.get_cities();
       unsigned len = city_vec.size();
       Cities::permutation_t permutation = city.random_permutation(len);
